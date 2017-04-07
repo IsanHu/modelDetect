f4853650f5b4b932306c586a8868f9a76f3b368f
isan <3isan333@gmail.com>
2017-04-07 16:51:32 +0800

    no message

--- app.py
+++ app.py
@@ -20,7 +20,5 @@ init_route(app)
 
 
 if __name__ == "__main__":
-    # app.run(debug=True)
-    app.run(host='0.0.0.0', debug=True)
-    # app.run(host='0.0.0.0', port=5555)
+    app.run(port=5555)
 
